201620ffe8482154339d643516a29a3290e2c295
Blackwaxx <guillaume.gicquel@yahoo.fr>
2018-07-15 23:55:58 +0200

    debug msg on driver_node

--- imibot_driver/src/diff_drive_control_handler.py
+++ imibot_driver/src/diff_drive_control_handler.py
@@ -19,22 +19,28 @@ class DiffDriveControlHandler:
     def move(self, msg):
         self.left_speed = msg.left_speed
         self.right_speed = msg.right_speed
+        print self.left_speed, self.right_speed
 
         if self.left_speed == 0 and self.right_speed == 0 :
             self.chassis.stop()
+        print 'stop'
         else:
             self.leftFreq = (self.servo_scale * (self.left_speed / 100)) + self.servo_min
             self.rightFreq = (self.servo_scale * (self.right_speed / 100)) + self.servo_min
 
             if self.left_speed > 0 and self.right_speed > 0:
+                print 'forward'
                 self.chassis.forward()
                 self.setSpeed()
             elif self.left_speed < 0 and self.right_speed > 0:
+                print 'left'
                 self.chassis.turnLeft()
                 self.setSpeed()
             elif self.left_speed > 0 and self.right_speed < 0:
+                print 'right'
                 self.chassis.turnRight()
                 self.setSpeed()
             elif self.left_speed < 0 and self.right_speed < 0:
+                print 'reverse'
                 self.chassis.reverse()
                 self.setSpeed()
