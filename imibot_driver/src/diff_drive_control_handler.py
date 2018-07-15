15df5b9e9e2f9a3a7c1e34bf515a6526705b0460
Blackwaxx <guillaume.gicquel@yahoo.fr>
2018-07-16 00:09:33 +0200

    test python node

--- imibot_driver/src/diff_drive_control_handler.py
+++ imibot_driver/src/diff_drive_control_handler.py
@@ -20,27 +20,34 @@ class DiffDriveControlHandler:
         self.left_speed = msg.left_speed
         self.right_speed = msg.right_speed
         print self.left_speed, self.right_speed
+        rospy.loginfo(self.left_speed)
+        rospy.loginfo(self.right_speed)
 
         if self.left_speed == 0 and self.right_speed == 0 :
             self.chassis.stop()
-        print 'stop'
+            print 'stop'
+            rospy.loginfo('stop')
         else:
             self.leftFreq = (self.servo_scale * (self.left_speed / 100)) + self.servo_min
             self.rightFreq = (self.servo_scale * (self.right_speed / 100)) + self.servo_min
 
             if self.left_speed > 0 and self.right_speed > 0:
                 print 'forward'
+                rospy.loginfo('forward')
                 self.chassis.forward()
                 self.setSpeed()
             elif self.left_speed < 0 and self.right_speed > 0:
                 print 'left'
+                rospy.loginfo('left')
                 self.chassis.turnLeft()
                 self.setSpeed()
             elif self.left_speed > 0 and self.right_speed < 0:
                 print 'right'
+                rospy.loginfo('right')
                 self.chassis.turnRight()
                 self.setSpeed()
             elif self.left_speed < 0 and self.right_speed < 0:
                 print 'reverse'
+                rospy.loginfo('reverse')
                 self.chassis.reverse()
                 self.setSpeed()
