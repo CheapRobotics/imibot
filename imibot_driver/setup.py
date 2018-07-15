77307729b433da2fbd3f51c90be0a27f32ebee21
Blackwaxx <guillaume.gicquel@yahoo.fr>
2018-07-15 22:25:15 +0200

    tryin to fix python node

---
+++ imibot_driver/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='src',
+    version='',
+    packages=['imibot', 'imibot.imibot_driver.src'],
+    url='',
+    license='',
+    author='yom',
+    author_email='',
+    description=''
+)
--- imibot_driver/src/diff_drive_chassis.py
+++ imibot_driver/src/diff_drive_chassis.py
@@ -1,4 +1,4 @@
-*import RPi.GPIO as GPIO
+import RPi.GPIO as GPIO
 import Adafruit_PCA9685
 
 
