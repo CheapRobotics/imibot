4632d1bacf30ffd45b24675fe7b637e0ad34f601
nbethuel <nicolasbethuel@gmail.com>
2018-07-16 11:58:10 +0200

    Add package imibot_camera, details about how to run it can be found in README

--- setup.sh
+++ setup.sh
@@ -1,1 +1,1 @@
-sudo apt install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-robot-localization ros-kinetic-twist-mux ros-kinetic-interactive-marker-twist-server ros-kinetic-joint-state-controller ros-kinetic-diff-drive-controller -y
+sudo apt install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-robot-localization ros-kinetic-twist-mux ros-kinetic-interactive-marker-twist-server ros-kinetic-joint-state-controller ros-kinetic-diff-drive-controller ros-kinetic-video-stream-opencv ros-kinetic-web-video-server -y
