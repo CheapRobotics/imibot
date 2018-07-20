ed0001386a06c651924113a41f112eab0f51dcac
Blackwaxx <guillaume.gicquel@yahoo.fr>
2018-07-20 21:54:07 +0200

    rearranged launch files

--- setup.sh
+++ setup.sh
@@ -1,1 +1,2 @@
-sudo apt install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-robot-localization ros-kinetic-twist-mux ros-kinetic-interactive-marker-twist-server ros-kinetic-joint-state-controller ros-kinetic-diff-drive-controller ros-kinetic-video-stream-opencv ros-kinetic-web-video-server -y
+#!/usr/bin/env bash
+sudo apt install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-robot-localization ros-kinetic-twist-mux ros-kinetic-interactive-marker-twist-server ros-kinetic-joint-state-controller ros-kinetic-diff-drive-controller ros-kinetic-video-stream-opencv ros-kinetic-web-video-server ros-kinetic-openni-launch -y
