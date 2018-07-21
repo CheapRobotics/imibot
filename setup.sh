7d0f439108d6408e6167815c712fa65e5c43f922
Blackwaxx <guillaume.gicquel@yahoo.fr>
2018-07-21 16:31:49 +0200

    starting map generation

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sudo apt install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-robot-localization ros-kinetic-twist-mux ros-kinetic-interactive-marker-twist-server ros-kinetic-joint-state-controller ros-kinetic-diff-drive-controller ros-kinetic-video-stream-opencv ros-kinetic-web-video-server ros-kinetic-openni-launch -y
+sudo apt install ros-kinetic-desktop-full ros-kinetic-controller-manager ros-kinetic-robot-localization ros-kinetic-twist-mux ros-kinetic-interactive-marker-twist-server ros-kinetic-joint-state-controller ros-kinetic-diff-drive-controller ros-kinetic-video-stream-opencv ros-kinetic-web-video-server ros-kinetic-openni-launch ros-kinetic-gmapping ros-kinetic-depthimage-to-laserscan ros-kinetic-amcl -y
